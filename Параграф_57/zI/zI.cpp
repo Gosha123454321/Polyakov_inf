c60de64cec4675c1be791db77e05c305a1d17cfd
Gosha123454321 <gosha.ladeyshchikov03@mail.rugit config --global user.name Gosha123454321git commit -am >
2021-02-11 13:47:32 +0500

    11.02.21

---
+++ Параграф_57/zI/zI.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	double x, y;
+	cin >> x >> y;
+	if (y < 1) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+	if (y < -x) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+	if ((x * x) + (y * y) < 1 * 1) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+	if ((x - 1)*(x - 1) + y * y < 1) {
+		cout << 1;
+	}
+	else {
+		cout << 0;
+	}
+	return 0;
+}
