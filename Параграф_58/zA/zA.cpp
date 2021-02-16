0bb1c1e9d79e3766fff13fb962f0c05aedddc8d7
Gosha123454321 <gosha.ladeyshchikov03@mail.rugit config --global user.name Gosha123454321git commit -am >
2021-02-16 15:37:32 +0500

    16.02.21

---
+++ Параграф_58/zA/zA.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c = 0;
+	cin >> a >> b;
+	for (int i = 0; i < abs(b); i++) {
+		c += abs(a);
+	}
+	if (a < 0 && b < 0 || a > 0 && b > 0) {
+		c = abs(c);
+	}
+	else {
+		c = -c;
+	}
+	cout << c;
+	return 0;
+}
