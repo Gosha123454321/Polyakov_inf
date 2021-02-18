659095505e69a20340135100a97c5632b56d1eee
Gosha123454321 <gosha.ladeyshchikov03@mail.rugit config --global user.name Gosha123454321git commit -am >
2021-02-18 16:52:24 +0500

    18.02.21

---
+++ Параграф_58/zF/zF.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, n;
+	cin >> a >> b >> n;
+	for (int i = 1; i <= n; i++) {
+		int с;
+		с = a + rand() % (b - a + 1);	
+		cout << n << ' ';
+	}
+	return 0;
+}
---
+++ Параграф_58/zG/zG.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	bool isDetected = false;
+	for (int i = 10000; i < 100000; i++) {
+		int x = i;
+		if (x % a == b && x % c == d) {
+			cout << x << ' ';
+			isDetected = true;
+		}
+	}
+	if (!isDetected) {
+		cout << -1;
+	}
+	return 0;
+}
