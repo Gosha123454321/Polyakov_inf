79604cd7395354178719df790b1d223e7034816d
Gosha123454321 <gosha.ladeyshchikov03@mail.rugit config --global user.name Gosha123454321git commit -am >
2021-03-03 17:19:35 +0500

    03.03.21

---
+++ Параграф_58/zN/zN.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, a;
+	cin >> n;
+	bool isTrue = true;
+	int last = n % 10;
+	while (n != 0) {
+		a = n % 10;
+		if (last != a) {
+			isTrue = false;
+		}
+		n /= 10;
+	}
+	if (isTrue == true) {
+		cout << "YES";
+	}
+	else {
+		cout << "NO";
+	}
+	return 0;
+}
